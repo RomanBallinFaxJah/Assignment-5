914bf9590605c5dba0d715a883e48201e3818a55
Roman-Daher <84515748+RomanBallinFaxJah@users.noreply.github.com>
2021-06-12 19:22:36 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById('button').addEventListener('click', getCircumferenceAndArea)
+
+// adjust how many decimal places it rounds to here
+const round = 3
+
+function getCircumferenceAndArea () {
+  // gets values and calculates area and circumference
+  const radius = document.getElementById('radius').value
+  circumference = radius * Math.PI * 2
+  area = radius ** 2 * Math.PI
+
+  // Displays the results of the calculations, and rounds to a number of decimal places with toFixed method and value set in the code
+  document.getElementById('area').innerHTML = `The area is ${area.toFixed(round)}`
+  document.getElementById('circumference').innerHTML = `The circumference is ${circumference.toFixed(round)}`
+}
